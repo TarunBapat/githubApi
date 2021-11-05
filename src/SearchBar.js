ec75996778a63423fe463e99f831a62c88ee4b00
TarunBapat <32245027+TarunBapat@users.noreply.github.com>
2021-11-05 18:07:03 +0530

    updated

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,10 +2,22 @@ import { useState } from "react";
 
 const SearchBar = () => {
   const [searchValue, setSearchValue] = useState("");
+  const [userData, setUserData] = useState([]);
   const searchDataHandler = (e) => {
     setSearchValue(e.target.value);
     console.log(searchValue);
   };
-  return <input type="text" value={searchValue} onChange={searchDataHandler} />;
+  const userDataHandler = async () => {
+    const response = await fetch(`https://api.github.com/users/${searchValue}`);
+    const data = await response.json();
+    setUserData(userData);
+    console.log(userData);
+  };
+  return (
+    <div>
+      <input type="text" value={searchValue} onChange={searchDataHandler} />
+      <button onClick={userDataHandler}>submit</button>
+    </div>
+  );
 };
 export default SearchBar;
