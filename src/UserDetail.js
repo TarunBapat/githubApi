a668ecdb7722dd37cafc7ef77eac1090616ba0b0
TarunBapat <32245027+TarunBapat@users.noreply.github.com>
2021-11-18 23:09:35 +0530

    updated

--- src/UserDetail.js
+++ src/UserDetail.js
@@ -1,11 +1,33 @@
-import react, { useContext } from "react";
+import react, { useContext, useState } from "react";
 import { GithubContext } from "./githubApiContext";
+import { Button } from "react-bootstrap";
 
 const UserDetail = () => {
   const { userData } = useContext(GithubContext);
+  const [repos, setRepos] = useState([]);
+  const fetchRepos = async () => {
+    const repo = await fetch(
+      `https://api.github.com/users/${userData.login}/repos`
+    );
+    const resp = await repo.json();
+    setRepos(resp);
+  };
   return (
     <div>
-      <img src={userData.avatar_url} />
+      <div className="container-left">
+        <img src={userData.avatar_url} />
+      </div>
+      <div className="container-right">
+        <h1>My Repos</h1>
+        <Button onClick={fetchRepos}>Fetch Repos</Button>
+        {repos.map((repo) => {
+          return (
+            <>
+              <p>{repo.name}</p>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 };
